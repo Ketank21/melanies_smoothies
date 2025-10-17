0d990f4037285e67817531f946e16fc0edd9a1fd
Ketank21 <77642625+Ketank21@users.noreply.github.com>
2025-10-17 09:48:03 +0200

    Add Smoothiefroot API call to display watermelon data
    Reintroduce Smoothiefroot API call and display response.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,6 @@ import streamlit as st
 # from snowflake.snowpark.context import get_active_session # this line needed to run streamlit in Snowflake (SiS), for SniS(not) this is not needed
 from snowflake.snowpark.functions import col
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie:cup_with_straw:")
@@ -44,3 +42,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success(f'Your Smoothie is ordered,{name_on_order}', icon="✅")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
