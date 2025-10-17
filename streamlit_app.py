d3b57af7a941accd14aa00ec2c0c439b390865c9
Ketank21 <77642625+Ketank21@users.noreply.github.com>
2025-10-17 10:04:19 +0200

    Improve ingredient handling and API response display
    Refactor ingredient string concatenation and update API call.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,10 +26,14 @@ ingredients_list =st.multiselect(
 )
 
 if ingredients_list:
+  
     ingredients_string =''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen
+      ingredients_string += fruit_chosen + ' '
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+      sf_df = st.dataframe(data = smoothiefroot_response.json(),use_container_width = True)
+      
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
@@ -43,6 +47,4 @@ if ingredients_list:
         
         st.success(f'Your Smoothie is ordered,{name_on_order}', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data = smoothiefroot_response.json(),use_container_width = True)
+
